59216f3670bfa117ad1cd6acc50ec4beae88ce0e
testpython1234589-dev <testpython1234589@gmail.com>
2026-03-13 15:07:51 +0100

    Update word_backend.py

--- word_backend.py
+++ word_backend.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Set, Dict, Any
 from datetime import datetime
-
 from docxtpl import DocxTemplate
 
 BASE_DIR = Path(__file__).resolve().parent
